5853abe4435742486809f515e76c603be53bf176
Pokhvalenko <vsevolod.pohvalenko@gmail.com>
2021-01-30 15:25:13 +0200

    hello

--- backend/views.py
+++ backend/views.py
@@ -1,4 +1,4 @@
 from django.views.decorators.cache import never_cache
 from django.views.generic import TemplateView
-
+#adsdasd
 index = never_cache(TemplateView.as_view(template_name='index.html'))
